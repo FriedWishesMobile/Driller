60fefa3734c94fa48b5dff09396f73220686248e
Philippe Casgrain <philippe@casgrain.com>
2010-09-15 18:50:26 -0400

    Initial version

---
+++ Classes/DrillerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  DrillerAppDelegate.h
+//  Driller
+//
+//  Created by Philippe on 10-09-15.
+//  Copyright Philippe Casgrain 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrillerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Driller
+//
+//  Created by Philippe on 10-09-15.
+//  Copyright Philippe Casgrain 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
