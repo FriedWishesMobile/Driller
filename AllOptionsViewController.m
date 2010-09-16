4967898e15381c88219ec4fc1c7fdf843a7ac779
Philippe Casgrain <philippe@casgrain.com>
2010-09-15 20:47:14 -0400

    Actual navigation!

--- AllOptionsViewController.m
+++ AllOptionsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AllOptionsViewController.h"
+#import "DrillDetailViewController.h"
 
 
 @implementation AllOptionsViewController
@@ -132,13 +133,16 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     // Navigation logic may go here. Create and push another view controller.
-	/*
-	 <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-	 [self.navigationController pushViewController:detailViewController animated:YES];
-	 [detailViewController release];
-	 */
+	DrillDetailViewController *detailViewController = [[DrillDetailViewController alloc] init];
+
+    detailViewController.title = [allOptions objectAtIndex:indexPath.row];
+
+    detailViewController.string = [allOptions objectAtIndex:indexPath.row];
+
+    [self.navigationController pushViewController:detailViewController animated:YES];
+
+    [detailViewController release];
+
 }
 
 
